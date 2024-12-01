10ebbc4407634898340dad370f14a0d67149801f
saikrishna <munagasaikrishna0420@gmail.com>
2024-12-01 10:29:34 +0530

    i commited

---
+++ Arthemtic_Operation/src/com/arth/Calculator.java
@@ -0,0 +1,16 @@
+package com.arth;
+
+public class Calculator {
+	
+	public static void main(String[] args) {
+		
+		int out=sumOfTwoNumbers(1, 2);
+		System.out.println(out);
+		
+	}
+	
+	public static int sumOfTwoNumbers(int n1,int n2) {
+		return n1+n2;
+	}
+
+}
