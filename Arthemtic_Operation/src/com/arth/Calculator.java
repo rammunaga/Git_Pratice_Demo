2768dadbbe4a4192c4f4bd77b919a98496158146
saikrishna <munagasaikrishna0420@gmail.com>
2024-12-01 11:08:40 +0530

    commited by vscode

--- Arthemtic_Operation/src/com/arth/Calculator.java
+++ Arthemtic_Operation/src/com/arth/Calculator.java
@@ -7,10 +7,26 @@ public class Calculator {
 		int out=sumOfTwoNumbers(1, 2);
 		System.out.println(out);
 		
+		System.out.println(isPrime(7));
 	}
 	
 	public static int sumOfTwoNumbers(int n1,int n2) {
 		return n1+n2;
+	}
+	
+	public static boolean isPrime(int n){
+		int i,f=0;
+		for(i=2;i<=n/2;i++){
+			if(n%i==0){
+				f=1;
+				break;
+			}
+		}
+		if(f==0){
+			return true;
+		}else{
+			return false;
+		}
 	}
 
 }
